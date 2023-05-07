b6905adf54efbf638565ffb102483b3b308a538a
Konrad <konslo96@gmail.com>
2023-05-07 17:00:52 +0200

    install cors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 
 const commentsRoutes = require("./routes/commentsRoutes");
 
 const app = express();
 const port = 4000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("<h1>Server is running</h1>");
