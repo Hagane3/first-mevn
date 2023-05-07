4a9f950007c41f9614dc3e205c1c5f54e2e9dfe9
Konrad <konslo96@gmail.com>
2023-05-07 17:13:31 +0200

    fetch items from api

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,9 +1,43 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref, reactive } from "vue";
+
+const state = reactive({
+  allComments: [],
+});
+
+onMounted(() => {
+  fetch("http://localhost:4000/comments")
+    .then((response) => response.json())
+    .then((data) => {
+      data.map((item) => state.allComments.push(item));
+    })
+    .catch((err) => console.log(err));
+});
+</script>
 
 <template>
   <div class="page">
-    <h1>Home page</h1>
+    <ul>
+      <li
+        class="comment_item"
+        v-for="comment in state.allComments"
+        :key="comment.title"
+      >
+        <h4>{{ comment.title }}</h4>
+        <h5>{{ comment.description }}</h5>
+      </li>
+    </ul>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+ul {
+  display: flex;
+  flex-direction: column;
+  gap: 2em;
+  margin-top: 3em;
+}
+.comment_item {
+  list-style: none;
+}
+</style>
